2d52328c71df1552b00d96deffc1d6b52aa766b4
andrealuzzi <89699775+andrealuzzi@users.noreply.github.com>
2026-02-15 10:57:49 +0100

    new description

--- src/Trading.jsx
+++ src/Trading.jsx
@@ -175,7 +175,8 @@ const Trading = () => {
       Whether it's web, cloud or multi-platform development, 
       we're here to empower your decisions and safeguard your loved ones' futures. 
       Our expertise ranges from multi-platform development through data integration 
-      to data analytics. From time to time, we make money for ourselves using our strategies
+      to data analytics. From time to time, we make money for ourselves using our strategies, particularly "SEESAW": 
+      a systematic long single stock short momentum sell offs vs indices
     </p>
   </div>
 </div>
